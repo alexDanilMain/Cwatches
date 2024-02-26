0fe4125451f564b1276c52f057205990c4abc6f6
alexdanilwork <alexdanilwork@gmail.com>
2024-02-26 22:07:48 +0100

    refactored Landing flex blox

--- src/Components/Landing/Landing.tsx
+++ src/Components/Landing/Landing.tsx
@@ -1,4 +1,5 @@
 import Container from "../../Elements/Container";
+import LandingFlexBlox from "../../Elements/LandingElements/LandingFlexBox";
 import ArmWithClock from "../../imgs/landing.images/ArmWithClock.png";
 import GrayStar from "../../imgs/landing.images/GrayStar.png";
 
@@ -6,15 +7,7 @@ export default function Landing() {
   return (
     <div className="h-[995px] w-full relative overflow-hidden">
       <Container>
-        <div className="flex flex-col text-white mt-52 gap-20">
-          <h1 className="text-[50px] max-w-72"> CUSTOMIZE YOUR TIME! </h1>
-          <p className=" max-w-[540px] text-[22px]">
-            Explore endless customization options and create a timepiece that
-            speaks to your personality. Your time, your design - only at
-            CustomWatches.
-          </p>
-          <button className="w-72 h-16 bg-white rounded-full text-black text-2xl font-bold"> Customise It</button>
-        </div>
+            <LandingFlexBlox />
       </Container>
       <img
         src={ArmWithClock}
---
+++ src/Elements/LandingElements/LandingFlexBox.tsx
@@ -0,0 +1,16 @@
+function LandingFlexBlox() {
+  return (
+    <div className="flex flex-col text-white mt-52 gap-20">
+      <h1 className="text-[50px] max-w-72"> CUSTOMIZE YOUR TIME! </h1>
+      <p className=" max-w-[540px] text-[22px]">
+        Explore endless customization options and create a timepiece that speaks
+        to your personality. Your time, your design - only at CustomWatches.
+      </p>
+      <button className="w-72 h-16 bg-white rounded-full text-black text-2xl font-bold">
+        Customise It
+      </button>
+    </div>
+  );
+}
+
+export default LandingFlexBlox;
